7844ddbc5780e8013ee273e1f87ad96624cc25e9
Rachel P <yanyan@Rachels-MacBook-Pro-2.local>
2023-12-07 15:58:50 -0500

    Async

--- screens/Add.js
+++ screens/Add.js
@@ -1,205 +1,240 @@
 import React, { useState } from 'react';
 import { Button, TextInput, StyleSheet, Text, View, Image, TouchableOpacity, KeyboardAvoidingView, Platform } from 'react-native';
 import { LinearGradient } from "expo-linear-gradient";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 function Add({ navigation }) {
-    
-    const[word, setWord] = useState();
-    const[definition, setDef] = useState();
-    const[name, setName] = useState();
 
-  const[listData, setListData] = useState([]);
+  // // const handleAdd = () => {
+  // //   setListData([...listData, {word, definition}])
+  // //   setWord(null)
+  // //   setDef(null)
+  // // }
+  const getData = async () =>{
+    const values = await AsyncStorage.multiGet("@name")
+    values.forEach(value =>{
+      if(value[0] === '@name'){
+        setName(JSON.parse(value[1]));
+      }else if(value[1] ==='@word'){
+
+      }
+    })
+  }
   
-    return (
-    
-        <LinearGradient style = {styles.container}
-            colors= {["#08204f", "#92e8f1"]}>
-            <Image source = {require('../assets/swiftDeck.png')}
-                    style = {styles.head_logo}></Image>
-            
-            <View style = {styles.box}>
-                <KeyboardAvoidingView enabled = {true} keyboardVerticalOffset={10}>
+  useEffect(getData);
+  const saveTitle = async () =>{
+    try{
+      AsyncStorage.setItem('title', text);
+    }catch(error){
+      console.log(error)
+    }
+  }
+  const getTitle = async ()=> {
+    try{
+      const title = await AsyncStorage.getItem('title');
+      setName(title);
+    }catch(error){}
+  }
 
-                <TextInput multiline= {true} style = {styles.title}
-                            value={name}
-                            placeholder='Title of the Deck'
-                            onChangeText={(newText)=>{
-                            setName(newText);}}/>
-                             
-                <TextInput multiline= {true} style = {styles.term}
-                            value={word}
-                            placeholder='Enter Term'
-                            onChangeText={(newText)=>{
-                            setWord(newText);}}/>
+  
 
-                <TextInput multiline= {true} style = {styles.definition}
-                            value = {definition}
-                            placeholder='Enter Definition'
-                            onChangeText={(newText)=>{
-                            setDef(newText);}}/>
-               </KeyboardAvoidingView>
+  const[word, setWord] = useState();
+  const[definition, setDef] = useState();
+  const[name, setName] = useState();
 
-                <TouchableOpacity onPress ={()=>{listData.push({title: name, term: word, def: definition});navigation.navigate("Creates", listData);}}>
-                    <View style = {styles.createB}>
-                        <Text style = {{color: "#FFF" ,textAlign: 'center', fontFamily: 'Gill Sans', top: 10, fontSize: 30, fontWeight: 'bold'}}>
-                            Create
-                        </Text>
-                    </View>
-                </TouchableOpacity>
-                <TouchableOpacity onPress = {() => {navigation.navigate("Home", listData)}}>
-                  <View style = {styles.backB}>
-                    <Text style = {{color: "#FFF" ,textAlign: 'center', fontFamily: 'Gill Sans', top: 5, fontSize: 15, fontWeight: 'bold'}}>BACK</Text>
+const[listData, setListData] = useState([]);
+
+  return (
+  
+      <LinearGradient style = {styles.container}
+          colors= {["#08204f", "#92e8f1"]}>
+          <Image source = {require('../assets/swiftDeck.png')}
+                  style = {styles.head_logo}></Image>
+          
+          <View style = {styles.box}>
+              <KeyboardAvoidingView enabled = {true} keyboardVerticalOffset={10}>
+
+              <TextInput multiline= {true} style = {styles.title}
+                          value={name}
+                          placeholder='Title of the Deck'
+                          onChangeText={(newText)=>{
+                          setName(newText);}}/>
+                           
+              <TextInput multiline= {true} style = {styles.term}
+                          value={word}
+                          placeholder='Enter Term'
+                          onChangeText={(newText)=>{
+                          setWord(newText);}}/>
+
+              <TextInput multiline= {true} style = {styles.definition}
+                          value = {definition}
+                          placeholder='Enter Definition'
+                          onChangeText={(newText)=>{
+                          setDef(newText);}}/>
+             </KeyboardAvoidingView>
+
+              <TouchableOpacity onPress ={()=>{listData.push({title: name, term: word, def: definition});navigation.navigate("Creates", listData);}}>
+                  <View style = {styles.createB}>
+                      <Text style = {{color: "#FFF" ,textAlign: 'center', fontFamily: 'Gill Sans', top: 10, fontSize: 30, fontWeight: 'bold'}}>
+                          Create
+                      </Text>
                   </View>
-                </TouchableOpacity>
-                
-            </View>
-        
-        </LinearGradient>
+              </TouchableOpacity>
+              <TouchableOpacity onPress = {() => {saveTitle(); navigation.navigate("Home", listData)}}>
+                <View style = {styles.backB}>
+                  <Text style = {{color: "#FFF" ,textAlign: 'center', fontFamily: 'Gill Sans', top: 5, fontSize: 15, fontWeight: 'bold'}}>BACK</Text>
+                </View>
+              </TouchableOpacity>
+              
+          </View>
+      
+      </LinearGradient>
 
-  );
+);
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    box:{
-        borderRadius: 10,
-        // borderWidth: 1,
-        width: 300,
-        height: 450,
-        backgroundColor: '#a2d8f7',
-        top: 50,
-        right: 0,
-        borderWidth:1,
-        borderColor:"white",
-    },
-    head_logo:{
-        position: 'absolute',
-        flex:1,
-        width: 400,
-        height: 400,
-        left: -5,
-        resizeMode: 'contain',
-        bottom: 515,
-    },
-    sets:{
-        borderRadius: 10,
-        backgroundColor: '#a2d8f7',
-        width: 350,
-        height: 150,
-        left:20,
-        marginBottom: 10,
-        flexDirection: 'row',
-        
-      },
-
-    create:{
+  container: {
+    flex: 1,
+    
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box:{
       borderRadius: 10,
       // borderWidth: 1,
-      width: 350,
-      height: 500,
-      top: 200,
+      width: 300,
+      height: 450,
       backgroundColor: '#a2d8f7',
-      position: "absolute",
+      top: 50,
+      right: 0,
+      borderWidth:1,
+      borderColor:"white",
+  },
+  head_logo:{
+      position: 'absolute',
+      flex:1,
+      width: 400,
+      height: 400,
+      left: -5,
+      resizeMode: 'contain',
+      bottom: 515,
+  },
+  sets:{
+      borderRadius: 10,
+      backgroundColor: '#a2d8f7',
+      width: 350,
+      height: 150,
+      left:20,
+      marginBottom: 10,
+      flexDirection: 'row',
+      
     },
 
-    each:{
-        paddingHorizontal: 10,
-        borderRadius: 10,
-        // borderWidth:1,
-        width:300,
-        height: 130,
-        backgroundColor: "#b8e3ff",
-        right:50,
-        top: 50,
-       position:'absolute', 
-        fontFamily: "Gill Sans",
-        fontSize: 30,
-        
-        paddingTop: 20,
-        
-      },
+  create:{
+    borderRadius: 10,
+    // borderWidth: 1,
+    width: 350,
+    height: 500,
+    top: 200,
+    backgroundColor: '#a2d8f7',
+    position: "absolute",
+  },
 
-      title:{
-        paddingHorizontal: 10,
-        borderRadius: 10,
-        
-        // borderWidth:1,
-        width:280,
-        height:25,
-        backgroundColor: "#b8e3ff",
-        left:10,
-        top: 30,
-        color: "#FFF",
-        fontFamily: "Gill Sans",
-        fontSize: 15,
-        borderWidth:1,
-        borderColor:"white",
-        marginBottom:-25,
-        
-        
-      },
+  each:{
+      paddingHorizontal: 10,
+      borderRadius: 10,
+      // borderWidth:1,
+      width:300,
+      height: 130,
+      backgroundColor: "#b8e3ff",
+      right:50,
+      top: 50,
+     position:'absolute', 
+      fontFamily: "Gill Sans",
+      fontSize: 30,
+      
+      paddingTop: 20,
+      
+    },
 
-      term:{
-        paddingHorizontal: 10,
-        borderRadius: 10,
-        
-        // borderWidth:1,
-        width:280,
-        height:110,
-        backgroundColor: "#b8e3ff",
-        left:10,
-        top: 90,
-        color: "#FFF",
-        fontFamily: "Gill Sans",
-        fontSize: 25,
-        borderWidth:1,
-        borderColor:"white",
-        
-        
-      },
+    title:{
+      paddingHorizontal: 10,
+      borderRadius: 10,
+      
+      // borderWidth:1,
+      width:280,
+      height:25,
+      backgroundColor: "#b8e3ff",
+      left:10,
+      top: 30,
+      color: "#FFF",
+      fontFamily: "Gill Sans",
+      fontSize: 15,
+      borderWidth:1,
+      borderColor:"white",
+      marginBottom:-25,
+      
+      
+    },
 
-      definition:{
-        paddingHorizontal: 10,
-        borderRadius: 10,
-        // borderWidth:1,
-        width:280,
-        height: 110,
-        backgroundColor: "#b8e3ff",
-        left:10,
-        top: 120,
-        color: "#FFF",
-        fontFamily: "Gill Sans",
-        fontSize: 25,
-        borderWidth:1,
-        borderColor:"white",
-        
-        
-      },
+    term:{
+      paddingHorizontal: 10,
+      borderRadius: 10,
+      
+      // borderWidth:1,
+      width:280,
+      height:110,
+      backgroundColor: "#b8e3ff",
+      left:10,
+      top: 90,
+      color: "#FFF",
+      fontFamily: "Gill Sans",
+      fontSize: 25,
+      borderWidth:1,
+      borderColor:"white",
+      
+      
+    },
 
-      createB:{
-        borderRadius:10,
-        width:180,
-        height: 50,
-        backgroundColor: "#b8e3ff",
-        top: 150,
-        right:60,
-        position:'absolute'
-        
-      },
+    definition:{
+      paddingHorizontal: 10,
+      borderRadius: 10,
+      // borderWidth:1,
+      width:280,
+      height: 110,
+      backgroundColor: "#b8e3ff",
+      left:10,
+      top: 120,
+      color: "#FFF",
+      fontFamily: "Gill Sans",
+      fontSize: 25,
+      borderWidth:1,
+      borderColor:"white",
+      
+      
+    },
 
-      backB:{
-        borderRadius:10,
-        width:210,
-        height: 30,
-        backgroundColor: "#b8e3ff",
-        top: 300,
-        position: 'absolute',
-        left:42,
-      },
+    createB:{
+      borderRadius:10,
+      width:180,
+      height: 50,
+      backgroundColor: "#b8e3ff",
+      top: 150,
+      right:60,
+      position:'absolute'
+      
+    },
+
+    backB:{
+      borderRadius:10,
+      width:210,
+      height: 30,
+      backgroundColor: "#b8e3ff",
+      top: 300,
+      position: 'absolute',
+      left:42,
+    },
 
 });
 
